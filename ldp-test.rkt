d029a7e327fbdafc38e8a6c4e92ea24e0506c75e
diegomirandahenriquez <32444316+diegomirandahenriquez@users.noreply.github.com>
2017-10-02 00:13:00 -0300

    Create ldp-test.rkt

---
+++ ldp-test.rkt
@@ -0,0 +1,13 @@
+#lang reader "ldp.rkt"
+new graf completo 3
+new nodo a
+new nodo s
+new nodo g
+new nodo 5
+new aris # g a 
+new aris ! a s 10
+new aris # s g 4
+del aris # a s
+del aris ! g s
+show graf
+show aris
